dadaea5dad2f8e121a4acab92dbf51c7ea4570d9
tsook <kimts0830@gmail.com>
2018-11-17 20:41:57 +0900

    Add get_user request handler

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,6 +27,24 @@ module.exports = function(app, con){
 		})
 	})
 
+	// get_user: get user info using user_id
+	app.get('/get_user', function(req, res){
+		con.query(`SELECT * FROM users WHERE user_id=${con.escape(req.query.id)}`, function(err, result){
+			if(err){
+				console.log(err)
+				res.send()
+				return
+			}
+
+			if(result.length == 0){
+				res.send({})
+				return
+			}
+
+			res.send(result[0])
+		})
+	})
+
 	// add_user: given android_id, user_id, color_1, color_2 add user information to database
 	app.post('/add_user', function(req, res){
 		var android_id = con.escape(req.body.android_id)
