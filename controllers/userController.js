6f615bc51ae373f719e7b59b05fdde8afb211356
tsook <kimts0830@gmail.com>
2018-11-19 00:33:56 +0900

    Fix confusion between APIs

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,13 +3,8 @@ module.exports = function(app, con){
 	// user_info: get all basic information of user given 'id' android_id
 	app.get('/user_info', function(req, res){
 		con.query(`SELECT * FROM users WHERE android_id=${con.escape(req.query.id)}`, function(err, result){
-			if(err){
+			if(err || result.length == 0){
 				console.log(err)
-				res.send()
-				return
-			}
-
-			if(result.length == 0){
 				res.send({})
 				return
 			}
@@ -23,32 +18,18 @@ module.exports = function(app, con){
 				})
 			}
 
-			res.send(result[0])
-		})
-	})
+			var data = {userData: result[0], friends: [], sessions: []}
 
-	// get_user: get all user info using user_id
-	app.get('/get_user', function(req, res){
-		// Get user data
-		var data = {userData: {}, friends: [], sessions: []}
-		con.query(`SELECT * FROM users WHERE user_id=${con.escape(req.query.id)}`, function(err, result){
-			if(err || result.length == 0){
-				console.log(err)
-				res.send(data)
-				return
-			}
-
-			data.userData = result[0]
-
+			var userId = con.escape(data.userData.user_id)
 			// Get list of friends
-			var sql = `SELECT friend1_id, friend2_id FROM friends WHERE friend1_id=${con.escape(req.query.id)} OR friend2_id=${con.escape(req.query.id)}`
+			var sql = `SELECT friend1_id, friend2_id FROM friends WHERE friend1_id=${userId} OR friend2_id=${userId}`
 			con.query(sql, function(err, result){
 				if(err){
 					console.log(err)
 					res.send(data)
 					return
 				}
-				var friend_str = result.map(entry => entry.friend1_id === req.query.id ? con.escape(entry.friend2_id) : con.escape(entry.friend1_id)).join(', ')
+				var friend_str = result.map(entry => entry.friend1_id === data.userData.user_id ? con.escape(entry.friend2_id) : con.escape(entry.friend1_id)).join(', ')
 				con.query(`SELECT user_id, color_1, color_2, color_3, level FROM users WHERE user_id IN (${friend_str})`, function(err, result){
 					if(err){
 						console.log(err)
@@ -57,7 +38,7 @@ module.exports = function(app, con){
 					}
 
 					data.friends = result
-					var sql = `SELECT datetime, duration, distance, penalty FROM sessions WHERE user_id=${con.escape(req.query.id)}`
+					var sql = `SELECT datetime, duration, distance, penalty FROM sessions WHERE user_id=${userId}`
 					con.query(sql, function(err, result){
 						if(err){
 							console.log(err)
@@ -71,6 +52,20 @@ module.exports = function(app, con){
 
 				})
 			})
+		})
+	})
+
+	// get_user: get all user info using user_id
+	app.get('/get_user', function(req, res){
+		// Get user data
+		con.query(`SELECT * FROM users WHERE user_id=${con.escape(req.query.id)}`, function(err, result){
+			if(err || result.length == 0){
+				console.log(err)
+				res.send({})
+				return
+			}
+
+			res.send(result[0])
 		})
 	})
 
