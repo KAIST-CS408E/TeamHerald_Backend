d4f5f7910bcdfe794618a05fe969076129248a43
tsook <kimts0830@gmail.com>
2018-11-19 22:02:37 +0900

    Change add_session API to support android id

--- controllers/bikeController.js
+++ controllers/bikeController.js
@@ -2,36 +2,47 @@ module.exports = function(app, con){
 
 	// add_session: add session information to database and update energy levels
 	app.post('/add_session', function(req, res){
-		var user_id = con.escape(req.body.user_id)
-		var distance = req.body.distance
-		var duration = req.body.duration
-		var penalties_str = con.escape('[' + req.body.penalties.map(entry => con.escape(entry)).join(', ') + ']')
-
-		var sql = `INSERT INTO sessions (user_id, duration, distance, penalty) VALUES (${user_id}, ${distance}, ${duration}, ${penalties_str})`
+		var android_id = con.escape(req.body.android_id)
+		var sql = `SELECT user_id FROM users WHERE android_id=${android_id}`
 		con.query(sql, function(err, result){
-			if(err){
+			if(err || result.length == 0){
 				console.log(err)
 				res.send({success: false})
 				return
 			}
-			var changePowerStr = "power"
-			if(req.body.duration >= 300 & req.body.penalties.length == 0)
-				changePowerStr = "IF(power+2 > 10, 10, power+2)"
-			else if(req.body.penalties.length > 0)
-				changePowerStr = "IF(power-2 < 1, 1, power-2)"
 
-			// Energy: 20 points per 5 mins of biking - 10 points per penalty
-			var new_energy = Math.floor(20 * duration/300) - 10 * req.body.penalties.length
-			if(new_energy < 0) new_energy = 0
-			var sql = `UPDATE users SET energy=IF(energy+${new_energy}>100, 100, energy+${new_energy}), power=${changePowerStr} WHERE user_id=${user_id}`
+			var user_id = con.escape(result[0].user_id)
+
+			var distance = req.body.distance
+			var duration = req.body.duration
+			var penalties_str = con.escape('[' + req.body.penalties.map(entry => con.escape(entry)).join(', ') + ']')
+
+			var sql = `INSERT INTO sessions (user_id, duration, distance, penalty) VALUES (${user_id}, ${distance}, ${duration}, ${penalties_str})`
 			con.query(sql, function(err, result){
 				if(err){
 					console.log(err)
 					res.send({success: false})
 					return
 				}
+				var changePowerStr = "power"
+				if(req.body.duration >= 300 & req.body.penalties.length == 0)
+					changePowerStr = "IF(power+2 > 10, 10, power+2)"
+				else if(req.body.penalties.length > 0)
+					changePowerStr = "IF(power-2 < 1, 1, power-2)"
 
-				res.send({success: true})
+				// Energy: 20 points per 5 mins of biking - 10 points per penalty
+				var new_energy = Math.floor(20 * duration/300) - 10 * req.body.penalties.length
+				if(new_energy < 0) new_energy = 0
+				var sql = `UPDATE users SET energy=IF(energy+${new_energy}>100, 100, energy+${new_energy}), power=${changePowerStr} WHERE user_id=${user_id}`
+				con.query(sql, function(err, result){
+					if(err){
+						console.log(err)
+						res.send({success: false})
+						return
+					}
+
+					res.send({success: true})
+				})
 			})
 		})
 	})
